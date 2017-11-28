bddc0a9b087d5530884698da1aa82ad2ffc19b6d
ksenia <ksenia.timofeeva@gmail.com>
2017-11-29 00:54:09 +0300

    MessageType enum changed

--- src/main/java/com/tsystems/app/logisticscommon/MessageType.java
+++ src/main/java/com/tsystems/app/logisticscommon/MessageType.java
@@ -4,29 +4,15 @@ package com.tsystems.app.logisticscommon;
  * Created by ksenia on 14.11.2017.
  */
 public enum MessageType {
-    GENERAL("GENERAL"),
-    ADD_DRIVER("ADD_DRIVER"),
-    UPDATE_DRIVER("UPDATE_DRIVER"),
-    DELETE_DRIVER("DELETE_DRIVER"),
-    ADD_TRUCK("ADD_TRUCK"),
-    UPDATE_TRUCK("UPDATE_TRUCK"),
-    DELETE_TRUCK("DELETE_TRUCK"),
-    ADD_ORDER("ADD_ORDER"),
-    UPDATE_ORDER("UPDATE_ORDER"),
-    DELETE_ORDER("DELETE_ORDER");
-
-    private final String name;
-
-    MessageType(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return this.getName();
-    }
+    GENERAL,
+    ADD_DRIVER,
+    UPDATE_DRIVER,
+    DELETE_DRIVER,
+    ADD_TRUCK,
+    UPDATE_TRUCK,
+    DELETE_TRUCK,
+    ADD_ORDER,
+    UPDATE_ORDER,
+    UPDATE_ORDER_BY_DRIVER_LOGIN,
+    DELETE_ORDER
 }
